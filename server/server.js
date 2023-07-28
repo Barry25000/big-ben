83f4ad4ea59f9e05e11b75808674b84532725a90
Barry25000 <barryengler@gmail.com>
2023-07-28 10:43:08 -0500

    package jason update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
